894189f3a4c3371b4ade3486608e01a27529f118
c-holmes <chrisrobholmes@gmail.com>
2017-09-14 08:13:22 -0400

    updated webpack config for prod build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-// TODOs
-// update design
+// registerServiceWorker();
+// Service workers are only available to "secure origins" (HTTPS sites, basically)
--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-const nodeEnv = process.env.NODE_ENV || 'production';
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	devtool: 'source-map',
 	//where you want to start
 	entry: {
 		filename: './src/index.js',
@@ -25,28 +23,13 @@ module.exports = {
 				},
 			},
 			// scss
-			{ // sass / scss loader for webpack
+			{
 			  test: /\.(sass|scss)$/,
 			  loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
 			}
 		],
 	},
-	devServer: {
-	  contentBase: path.join(__dirname, "public"),
-	  compress: true,
-	  port: 9000
-	},
 	plugins: [
-		// uglifyjs
-		new webpack.optimize.UglifyJsPlugin({
-			compress: { warnings: false },
-			output: { comments: false },
-			sourceMap: true,
-		}),
-		// env plugin
-		new webpack.DefinePlugin({
-			'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
-		}),
 		new ExtractTextPlugin('public/style.css', {
 		  allChunks: true
 		})
