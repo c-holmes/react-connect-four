fa3d1b27fffbb339e977ddf6cce052513d39840b
c-holmes <chrisrobholmes@gmail.com>
2017-10-16 07:21:41 -0400

    stash updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Game from './Game';
-import GameMenu from './GameMenu';
+// import GameMenu from './GameMenu';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      location: 'home-screen',
+      multiplayer: false
+    }
+  }
+
+  ParentClassNameBy(url){
+    if(url === '/'){
+      return 'home-screen';
+    } else {
+      return 'misc-screen';
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path='/' component={GameMenu}/>
-          <Route path='/play' component={Game}/>
-          <Route path='/lobby' component={Game}/>
-        </Switch>
+        <Router>
+          <div>
+              <ul className={this.state.location}>
+                <li><Link to="/" className="home">Home</Link></li>
+                <li><Link to="/play" className="play">Single Player</Link></li>
+                <li><Link to="/lobby" className="lobby">Multiplayer</Link></li>
+                <li><Link to="/how-to-play" className="how-to-play">How To Play</Link></li>
+              </ul>
+              <Route path='/play' component={Game} multiplayer={this.state.multiplayer} />
+              <Route path='/lobby' component={Game}/>
+          </div>
+        </Router>
       </div>
     );
   }
--- src/GameMenu.js
+++ src/GameMenu.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Game from './Game';
+
 
 class GameMenu extends Component {
 	render() {
 		return(
 			<div className="game-menu">
-				this is Home menu
+				<Router>
+				  <div>
+				      <ul className="game-menu">
+				        <li><Link to="/" className="home" >Home</Link></li>
+				        <li><Link to="/play" className="play" >Single Player</Link></li>
+				        <li><Link to="/lobby" className="lobby" >Multiplayer</Link></li>
+				        <li><Link to="/how-to-play" className="how-to-play">How To Play</Link></li>
+				      </ul>
+				      <Route path='/play' component={Game} />
+				      <Route path='/lobby' component={Game}/>
+				  </div>
+				</Router>
 			</div>
 		)
 	}
