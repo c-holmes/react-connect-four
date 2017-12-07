1a463c7d0f109995a8855b3b1b2bd3a0434eb7de
c-holmes <chrisrobholmes@gmail.com>
2017-12-06 22:32:28 -0500

    set height dynamically

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -3,11 +3,38 @@ import { Link } from 'react-router';
 import shortid from 'shortid';
 
 class GameMenu extends Component {
+	constructor(){
+		super();
+		this.state = {
+			panelStyle: {
+				height: 'auto'
+			}
+		};
+	}
+
 	handleClick(gameId, multiplayer) {
 		this.props.gameStart(gameId, multiplayer);
 	}
 
+	componentDidMount() {
+		let gameMenu = document.getElementsByClassName('game-menu')[0];
+		let gameMenuMargin = parseInt(window.getComputedStyle(gameMenu, null).getPropertyValue('margin-top'));
+		this.setState({
+			panelStyle:{
+				height: window.innerHeight - (48 + (gameMenuMargin*2))
+			} 
+		});
+	}
+
 	render() {
+		// console.log(gameMenu);
+		// let gameMenuMargin = window.getComputedStyle(gameMenu, null).getPropertyValue('margin');
+		// console.log(gameMenuMargin);
+		// let autoHeight = {
+		// 	// height: window.outerHeight - (48 + gameMenuMargin)
+		// }
+		// console.log(autoHeight);
+		// console.log(gameMenu);
 		let gameId = shortid.generate();
 		return(
 		  <div>
@@ -18,13 +45,13 @@ class GameMenu extends Component {
 		  		</div>
 		      <ul className="game-menu">
 		        <li>
-		        	<Link to="/play" className="lobby">
+		        	<Link to="/play" className="lobby panel" style={this.state.panelStyle}>
 		        		<span className="top">Online Play</span>
 		        		<span className="bottom">Multiplayer</span> 
 		        	</Link>
 		        </li>
 		        <li>
-		        	<Link to={`/play/1-${gameId}`} className="play" onClick={()=>this.handleClick(`1-${gameId}`, false)} >
+		        	<Link to={`/play/1-${gameId}`} className="play panel" onClick={()=>this.handleClick(`1-${gameId}`, false)} style={this.state.panelStyle} >
 			        	<span className="top">Turn Based Play</span>
 			        	<span className="bottom">Single Device</span> 
 		        	</Link>
